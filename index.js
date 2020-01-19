87a4996091696cbe1d08664e6c941a3acecbd269
Carter <mike@mike.com>
2020-01-18 21:33:00 -0800

    changes

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const csv = require('csv-parser')
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const fs = require('fs')
-const axios = require('axios');
 const results = [];
 const appArray = [];
 
-this.state = {
-          termArray: 'asdf',
-          appVendorArray: []
-        }
+var AzureSearch = require('azure-search');
+var client = AzureSearch({
+  url: "x",
+  key: "x",
+  version: "2016-09-01", // optional, can be used to enable preview apis
+  headers: { // optional, for example to enable searchId in telemetry in logs
+    "x-ms-azs-return-searchid": "true",
+    "Access-Control-Expose-Headers": "x-ms-azs-searchid"
+
+  }
+});
 
 fs.createReadStream('input/ProvisionInput.csv')
   .pipe(csv(['Name']))
@@ -19,32 +25,101 @@ fs.createReadStream('input/ProvisionInput.csv')
 
     for (var i = 1; i < itemCount; i++)
     {
-          var appName = results[i][0]
-          var appStatus = results[i][1]
-          var appType = results[i][2]
-          var self = this;
-          var appInstalled = "No"
+          appArray.push({'name': results[i][0], 'status': results[i][1], 'type': results[i][2], 'installed': 'installed', 'servicerequest': 'servicerequest' })
+    }
 
-          appArray.push({'name': appName, 'status': appStatus, 'type': appType, 'installed': appInstalled})
+    getInstalledData()
 
+    //////
+
+    async function getInstalledData(){
+
+        var appCount = appArray.length;
+
+          for (var i = 0; i < appCount; i++)
+          {
+
+            await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+
+            await client.search('x', {search: appArray[i].name, top: 1}, function(err, searchResults){
+
+                if(searchResults.length > 0){
+                  installed(i-1, appArray[i-1].name, searchResults[0].questions[0])
+                }else{
+                  installed(i-1, appArray[i-1].name, "Not Installed")
+                }
+
+            });
+
+          }
+          getServiceRequestData()
+    }
+
+    //////
+
+    async function getServiceRequestData(){
+
+        var appCount = appArray.length;
+
+          for (var i = 0; i < appCount; i++)
+          {
+
+            await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+
+            await client.search('x', {search: appArray[i].name, top: 1}, function(err, searchResults){
+
+                if(searchResults.length > 0){
+                  ServiceRequest(i-1, appArray[i-1].name, searchResults[0].metadata_rawid)
+                }else{
+                  appServiceRequest = "No"
+                  ServiceRequest(i-1, appArray[i-1].name, "No RAW")
+                }
+
+            });
+
+
+          }
+
+        //////
+
+        await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+
+        const csvWriter = createCsvWriter({
+              path: 'output/ProvisionOutput.csv',
+              header: [
+                  {id: 'name', title: 'Name'},
+                  {id: 'status', title: 'Status'},
+                  {id: 'type', title: 'Type'},
+                  {id: 'installed', title: 'Installed'},
+                  {id: 'servicerequest', title: 'Service Request'}
+              ]
+          });
+
+        csvWriter.writeRecords(appArray)
+              .then(() => {
+                  console.log('...Done');
+              });
     }
 
 
-    const csvWriter = createCsvWriter({
-        path: 'output/ProvisionOutput.csv',
-        header: [
-            {id: 'name', title: 'Name'},
-            {id: 'status', title: 'Status'},
-            {id: 'type', title: 'Type'},
-            {id: 'installed', title: 'Installed'}
-        ]
-    });
+    //////
 
-    csvWriter.writeRecords(appArray)      // returns a promise
-        .then(() => {
-            console.log('...Done');
-        });
+    function installed(num, appName, flexeraName){
+      console.log("num: " + num)
+      console.log("appName: " + appName)
+      console.log("flexeraName: " + flexeraName)
 
+      appArray[num].installed = flexeraName
 
+    }
 
-  });
+    function ServiceRequest(num, appName, rawId){
+      console.log("num: " + num)
+      console.log("appName: " + appName)
+      console.log("rawId: " + rawId)
+
+      appArray[num].servicerequest = rawId
+
+    }
+
+});
